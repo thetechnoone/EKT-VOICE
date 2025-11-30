e07c8c3922af779e3a709c72f86983321043ad7f
seedplanter <brambleopong@gmail.com>
2025-11-30 21:36:22 +0100

    The first version of EKY VOICE

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const posts = []
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+});
+
+app.get("/postpage",(req,res)=>{
+    res.render("posts.ejs")
+});
+
+app.post("/newpost",(req,res)=>{
+    console.log(req.body);
+    console.log (posts.length);
+
+    const newPosts = {
+        id: ((posts.length) + 1),
+        subject: req.body.subject,
+        neighborhood: req.body.neighborhood,
+        content: req.body.content,
+        date: new Date()
+    };
+
+    posts.push(newPosts);
+    console.log(posts);
+
+    res.render("posts.ejs",{posts:posts});
+});
+
+
+app.listen(port,()=>
+console.log(`Server is running on port ${port}`) 
+);
