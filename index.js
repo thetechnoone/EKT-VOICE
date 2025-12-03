f35f343d9b7d0ee3477ebf07cffae035a84cab05
seedplanter <brambleopong@gmail.com>
2025-12-03 23:32:12 +0100

    This has the deletion function

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/postpage",(req,res)=>{
-    res.render("posts.ejs")
+    res.render("posts.ejs",{posts:posts})
 });
 
 app.post("/newpost",(req,res)=>{
-    console.log(req.body);
-    console.log (posts.length);
+    // console.log(req.body);
+    // console.log (posts.length);
 
     const newPosts = {
         id: ((posts.length) + 1),
@@ -29,18 +29,29 @@ app.post("/newpost",(req,res)=>{
         neighborhood: req.body.neighborhood,
         content: req.body.content,
         name: req.body.name,
-        
         date: new Date()
     };
 
     posts.push(newPosts);
-    console.log(posts);
+    // console.log(posts);
 
     res.render("posts.ejs",{posts:posts});
 });
 
 
+app.get("/diffpage/:id",(req,res)=>{
 
+const postID = parseInt(req.params["id"]);
+
+const findPost= posts.findIndex(({id})=> id === postID);
+
+posts.splice(findPost,1)
+
+console.log(posts);
+
+res.redirect("/postpage");
+
+});
 
 app.listen(port,()=>
 console.log(`Server is running on port ${port}`) 
